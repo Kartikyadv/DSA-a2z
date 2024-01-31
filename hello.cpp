1135240facdf042d4c9b32c18714a792eea9bb25
Kartik Yadav <ky5986390@gmail.com>
2024-01-31 07:29:18 +0530

    Largest & 2nd Largest ele in array

--- hello.cpp
+++ hello.cpp
@@ -1,16 +1,31 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int n = 5;
-    // cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        for (int z = 0; z <= i; z++)
-        {
-            cout << i+1;
+Q-2 2nd Largest Element in the Array
+
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    //THE MMOST OPTIMAL ONE
+    vector<int> ans;
+    int largest = a[0];
+    int slargest = -1;
+    int smallest = a[0];
+    int ssmallest = 10000;
+    for(int i=1;i<a.size();i++){
+        if(a[i]>largest){
+            slargest = largest;
+            largest = a[i];
         }
-        cout << endl;
+        else if(a[i]<largest && a[i]>slargest){
+            slargest = a[i];
+        }
     }
-    
-    return 0;
+    ans.push_back(slargest);
+    for(int i=0;i<a.size();i++){
+        if(a[i]<smallest){
+            ssmallest = smallest;
+            smallest = a[i];
+        }
+        else if(a[i]>smallest && a[i]<ssmallest){
+            ssmallest = a[i];
+        }
+    }
+    ans.push_back(ssmallest);
+    return {slargest,ssmallest};
 }
